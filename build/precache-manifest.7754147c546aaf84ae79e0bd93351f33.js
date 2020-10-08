49ffc09a74fe3151e3e2429fb9b6dac8d40b07a9
Flávio Silva <flavioandre.2001.9.@gmail.com>
2020-10-08 19:38:28 +0100

    Add Build

---
+++ build/precache-manifest.7754147c546aaf84ae79e0bd93351f33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "558e0620ecc578d8bd00703f855161b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "259a664d6f59f63164ba",
+    "url": "/static/css/main.13acedce.chunk.css"
+  },
+  {
+    "revision": "629c91e0850d300405d2",
+    "url": "/static/js/2.ade5461b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ade5461b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "259a664d6f59f63164ba",
+    "url": "/static/js/main.c9a29124.chunk.js"
+  },
+  {
+    "revision": "8bc0cc25731b75047ef5",
+    "url": "/static/js/runtime-main.5dfb163b.js"
+  }
+]);
