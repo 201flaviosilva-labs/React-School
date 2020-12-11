db4879c9feaf1766dbbe68320783c4033c7c1e35
Flávio Silva <flavioandre.2001.9.@gmail.com>
2020-12-11 17:28:22 +0000

    Add Build

---
+++ build/precache-manifest.6a03adc57061095188c06e10e3b0b221.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db785d14e95302e933432137df00d048",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf873e0f47621e92eace",
+    "url": "/static/css/main.62ed4275.chunk.css"
+  },
+  {
+    "revision": "8f8b64187a0b1964866c",
+    "url": "/static/js/2.5685aabc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5685aabc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf873e0f47621e92eace",
+    "url": "/static/js/main.f08d2a0a.chunk.js"
+  },
+  {
+    "revision": "8bc0cc25731b75047ef5",
+    "url": "/static/js/runtime-main.5dfb163b.js"
+  },
+  {
+    "revision": "22bc0a3641b912ef76a5829c33b87994",
+    "url": "/static/media/Nublado.22bc0a36.png"
+  },
+  {
+    "revision": "1f7dbf6e28487f14f3c95922023e1995",
+    "url": "/static/media/Sol.1f7dbf6e.png"
+  }
+]);
