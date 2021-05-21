3fb474da33d0ae3999978913ecd2f10a847fb457
Flavio Silva <flavioandre.2001.9.7@gmail.com>
2021-05-21 23:38:47 +0100

    Loading

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactRout from "./ReactRout";
+import { FadeLoader } from "react-spinners";
+
+import "./style.css";
 
 export default function Main() {
+	const [isLoad, setIsLoad] = useState(false);
+
+	useEffect(() => { setIsLoad(true); }, []);
+
 	return (
-		<main>
-			<ReactRout />
+		<main className="main">
+			{isLoad
+				? <ReactRout />
+				: <div className="InitialLoader">
+					<FadeLoader color={"#ffffff"} loading={isLoad} />
+				</div>}
 		</main>
 	)
 }
