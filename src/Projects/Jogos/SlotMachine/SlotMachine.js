b9871a8f35e443f65890d3ba907f7fa97a96aad4
Flávio Silva <flavioandre.2001.9.@gmail.com>
2021-06-03 23:13:17 +0100

    Init slot

--- src/Projects/Jogos/SlotMachine/SlotMachine.js
+++ src/Projects/Jogos/SlotMachine/SlotMachine.js
@@ -1,20 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Slot from "./Slot";
 
 export default function SlotMachine() {
-	const [play, setPlay] = useState(false);
+	const [inPlay, setInPlay] = useState(false);
+	const [actualIcon, setActualIcon] = useState(3);
+	// const icons = ["🍒", "🍉", "🍊", "🍓", "🍇", "🥝"];
+	const icons = ["A", "B", "C", "D", "E", "F"];
 
-	const list = ["🍒", "🍉", "🍊", "🍓", "🍇", "🥝"];
+	function startSpin() {
+		setInPlay(true);
+		play();
+	}
+
+	play();
+	function play() {
+		setInterval(() => {
+			const i = actualIcon + 1;
+			// const i = actualIcon > icons.length ? 0 : actualIcon + 1;
+			setActualIcon(i);
+		}, 1000);
+	}
+
+	useEffect(() => {
+		console.log(actualIcon);
+	}, [actualIcon])
 
 	return (
 		<div>
-			<Slot target={1}>
-				{
-					list.map((icon, index) => <div key={index}>{icon}</div>)
-				}
-			</Slot>
+			<div>{icons[actualIcon]}, {actualIcon}</div>
 
-			<button id="play-button" onClick={() => setPlay(!play)}>Play</button>
+			{/* <Slot>
+				{
+					icons.map((icon, index) => <div key={index}>{icon}</div>)
+				}
+			</Slot> */}
+
+			<button id="play-button" onClick={() => { if (!inPlay) startSpin() }}>Play</button>
 		</div>
 	)
 }
