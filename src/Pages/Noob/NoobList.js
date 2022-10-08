75534250e6691eecee0fec2b6c4dc08b45884e4b
201flaviosilva <flavioandre.2001.9.7@gmail.com>
2022-10-08 20:31:28 +0000

    fix build

--- src/Pages/Noob/NoobList.js
+++ src/Pages/Noob/NoobList.js
@@ -1,5 +1,4 @@
 import Counter from "./projects/Counter";
-import Form from "./projects/Form";
 import MagicWords from "./projects/MagicWords";
 import MemeGenerator from "./projects/MemeGenerator";
 import MultiLanguageApp from "./projects/MultiLanguageApp";
@@ -7,7 +6,6 @@ import Todo from "./projects/Todo";
 
 export const NOOB_LIST = [
 	Counter,
-	Form,
 	MagicWords,
 	MemeGenerator,
 	MultiLanguageApp,
